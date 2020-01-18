20836c167b8f0f97666d35bce38a6857f76cf11c
George Roman <george.roman.99@gmail.com>
2020-01-18 21:27:14 +0200

    Add page for doctors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './modules/home/home.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import {DepartmentsAndRoomsComponent} from "./modules/dashboard/departments-and-rooms/departments-and-rooms.component";
+import {DoctorsComponent} from "./modules/dashboard/doctors/doctors.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'dashboard', component: DashboardComponent },
-  {path: 'departments-and-rooms', component: DepartmentsAndRoomsComponent}
+  {path: 'departments-and-rooms', component: DepartmentsAndRoomsComponent},
+  {path: 'doctors', component: DoctorsComponent}
 ];
 
 @NgModule({
---
+++ client/src/app/shared/model/DoctorInformation.ts
@@ -0,0 +1,5 @@
+export class DoctorInformation {
+    id: number;
+    doctor: number;
+    working_schedule: string;
+}
