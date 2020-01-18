b53156ff6c51ce088358ac104fabe548f9a01301
alexresiga <resiga.alex@gmail.com>
2020-01-18 11:31:27 +0200

    add dept and rooms page ish

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -3,13 +3,35 @@ import { DashboardComponent } from './dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DepartmentsAndRoomsComponent } from './departments-and-rooms/departments-and-rooms.component';
+import {RouterModule} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   imports: [
     CommonModule,
-    NgbModule
+    NgbModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTabsModule
   ],
-  declarations: [DashboardComponent, NavbarComponent],
+  declarations: [DashboardComponent, NavbarComponent, DepartmentsAndRoomsComponent],
   exports: []
 })
 export class DashboardModule {}
