484b13c174d9e6b28820598d59e2acbeb2b71328
unknown <f.carmen402@yahoo.com>
2020-01-19 00:35:33 +0200

    conflict fix4

--- client/src/app/shared/model/Appointment.ts
+++ client/src/app/shared/model/Appointment.ts
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 export class Appointment {
     id: number;
     doctor: number;
     patient: number;
-    date : string ;
-    room : string;
-    approved : string;
-=======
-import {Room} from "./Room";
-
-export class Appointment {
-    id: number;
-    patient: number;
-    doctor: number;
-    date: Date;
-    room: Room;
->>>>>>> c0916c0ca88541004e345a0a92f0e70bb189f3ff
+    date: string;
+    room: string;
+    approved: string;
 }
