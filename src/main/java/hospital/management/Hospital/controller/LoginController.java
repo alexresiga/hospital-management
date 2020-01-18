7df0c157833ef648ef63fecc0d40650dbb5d01ab
Pleto <pleto@mail.com>
2020-01-18 21:30:03 +0200

    current user

--- src/main/java/hospital/management/Hospital/controller/LoginController.java
+++ src/main/java/hospital/management/Hospital/controller/LoginController.java
@@ -1,5 +1,6 @@
 package hospital.management.Hospital.controller;
 
+import hospital.management.Hospital.converter.UserConverter;
 import hospital.management.Hospital.dto.UserDto;
 import hospital.management.Hospital.model.ErrorMessage;
 import hospital.management.Hospital.model.Role;
@@ -7,11 +8,14 @@ import hospital.management.Hospital.repository.RoleRepository;
 import hospital.management.Hospital.service.RoleService;
 import hospital.management.Hospital.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @RestController
@@ -25,5 +29,12 @@ public class LoginController {
     @PostMapping("/api/register")
     public ErrorMessage register(@RequestBody UserDto data) {
         return userService.registerUser(data);
+    }
+
+    @GetMapping("/api/currentUser")
+    public UserDto currentUser(@AuthenticationPrincipal Principal principal) {
+        System.out.println(principal.getName());
+        System.out.println(userService.getUserByEmail(principal.getName()));
+        return (userService.getUserByEmail(principal.getName()));
     }
 }
